4284237b8a5d5e692342c1effe7881ae8fc37926
devdbreno <dbrenoti@gmail.com>
2021-06-17 20:37:59 -0300

    chore: finished elixir basic types

--- iniciando-em-elixir/basic_types.exs
+++ iniciando-em-elixir/basic_types.exs
@@ -1,1 +1,23 @@
-IO.puts("adsad")
+IO.puts("Printing an integer number:")
+IO.inspect(92)
+
+IO.puts("\nPrinting a float number:")
+IO.inspect(92.92)
+
+IO.puts("\nPrinting a string:")
+IO.inspect("Hi, i'm a string :)")
+
+IO.puts("\nPrinting an atom:")
+IO.inspect(:hi_im_an_atom)
+
+IO.puts("\nPrinting a list:")
+IO.inspect([1, 2, 3])
+
+IO.puts("\nPrinting a concatened list:")
+IO.inspect([1, 2, 3] ++ [4, 5, 6])
+
+IO.puts("\nPrinting a list while removing elements from it:")
+IO.inspect([1, 2, 3] -- [2, 3])
+
+IO.puts("Printing a list with merged values:")
+IO.inspect([1, "i'm a string!", :im_a_atom, 9.92])
